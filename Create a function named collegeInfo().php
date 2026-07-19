b49917afd26ad5edc77fdaa242a0dbf9a0f5869f
sarthakpandyaa <sarthakpandya894@gmail.com>
2026-07-19 15:51:48 +0530

    Add files via upload

---
+++ Create a function named collegeInfo().php
@@ -0,0 +1,12 @@
+<?php
+function collegeInfo()
+{
+    echo "<h3>College Information</h3>";
+    echo "College Name: ABC College<br>";
+    echo "Course Name: Bachelor of Computer Applications (BCA)<br>";
+    echo "Academic Year: 2025-2026";
+}
+
+// Function Call
+collegeInfo();
+?>
