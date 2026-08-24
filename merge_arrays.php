1f21f1eaf5d87d9dace863553b10a36c8f989eac
sarthakpandyaa <sarthakpandya894@gmail.com>
2026-08-24 20:43:26 +0530

    Add files via upload

---
+++ merge_arrays.php
@@ -0,0 +1,15 @@
+<?php
+
+$firstArray = [1, 2, 3, 4];
+$secondArray = [5, 6, 7, 8];
+
+$mergedArray = array_merge($firstArray, $secondArray);
+
+echo "First Array: ";
+print_r($firstArray);
+
+echo "Second Array: ";
+print_r($secondArray);
+
+echo "Merged Array: ";
+print_r($mergedArray);
