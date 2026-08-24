a867ed50bcf19b5984f6282e4004fd500bfa56f1
sarthakpandyaa <sarthakpandya894@gmail.com>
2026-08-24 20:45:51 +0530

    Add files via upload

---
+++ college_info.php
@@ -0,0 +1,7 @@
+<?php
+
+function collegeInfo() {
+    echo "College Name: Your College Name<br>";
+    echo "Course Name: Your Course Name<br>";
+    echo "Academic Year: Your Academic Year<br>";
+}
