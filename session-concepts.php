81746ea380a934c54de139df037257dc45a06cea
sarthakpandyaa <sarthakpandya894@gmail.com>
2026-08-24 20:51:13 +0530

    Add files via upload

---
+++ session-concepts.php
@@ -0,0 +1,10 @@
+<!--phase-1--> 
+<!--programing language -->
+<!--Session =PHP , Pthon , java -->
+
+<!-- Frameworks-->
+<!-- Token Passing Session = Laravel , Django , Flask ,jspark-->
+
+
+<!-- Corporates With Server -->
+<!--o auth factor authentication == Google , Microsoft , Facebook , Github -->
